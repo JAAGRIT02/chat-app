e4f3ea4afa3e4f20c520bb0f33c6a80ca3436886
JAAGRIT <jaagrit08@gmail.com>
2024-12-01 14:48:40 +0530

    initialized firebase in the project

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/compat/app';
+// Your web app's Firebase configuration
+const config = {
+  apiKey: 'AIzaSyApg4zGIo-djVmlj8qeB6-LvhrI4v3kauQ',
+  authDomain: 'web-chat-app-37077.firebaseapp.com',
+  projectId: 'web-chat-app-37077',
+  storageBucket: 'web-chat-app-37077.firebasestorage.app',
+  messagingSenderId: '980156425587',
+  appId: '1:980156425587:web:74443d2864b9c6e4895106',
+};
+
+// Initialize Firebase
+const app = initializeApp(config);
